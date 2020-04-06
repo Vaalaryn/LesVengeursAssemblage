4a2934217930acae2de16cef5b1f3341113bfef9
Vaala <brice.bitot@neuf.fr>
2020-04-06 14:42:07 +0200

    connexion

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,10 +7,18 @@ import java.util.*;
 
 import models.*;
 
+@With(Secure.class)
 public class Application extends Controller {
+
+    @Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            Civils user = Civils.find("byId", Security.connected()).first();
+            renderArgs.put("user", user.id);
+        }
+    }
 
     public static void index() {
         render();
     }
-
 }
