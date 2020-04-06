81bf1420b823a9ae3cb77293fa3dd2d8314be48f
MaatheN <contact.pro@mathieu-neveu.fr>
2020-04-06 16:42:37 +0200

    fonction displayUserInfo

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,16 +9,26 @@ import models.*;
 
 @With(Secure.class)
 public class Application extends Controller {
+    public static String userId;
 
     @Before
     static void setConnectedUser() {
         if(Security.isConnected()) {
             Civils user = Civils.find("byId", Security.connected()).first();
             renderArgs.put("user", user.id);
+            userId = user.id;
         }
     }
 
     public static void index() {
         render();
+    }
+
+    public static void displayUserInfo(String which){
+        switch (which){
+            case "id":
+                render(userId);
+                break;
+        }
     }
 }
