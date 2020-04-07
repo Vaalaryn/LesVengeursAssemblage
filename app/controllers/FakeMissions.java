1ca41f258e59cd99f7681ca0eb877b435f34a37c
MaatheN <contact.pro@mathieu-neveu.fr>
2020-04-07 18:44:58 +0200

    update FakeMissions

--- app/controllers/FakeMissions.java
+++ app/controllers/FakeMissions.java
@@ -7,20 +7,20 @@ import java.util.Date;
 public class FakeMissions {
     public static void index() {
         try {
-            new models.NatureMission("sauvetage",null);
-            new models.NatureMission("invasion",null);
-            new models.NatureMission("assassinat",null);
-            new models.NatureMission("cambriolage",null);
+            new models.NatureMission("sauvetage",null).save();
+            new models.NatureMission("invasion",null).save();
+            new models.NatureMission("assassinat",null).save();
+            new models.NatureMission("cambriolage",null).save();
         }catch (Exception eNature){
             System.out.println("Erreur Création Nature de mission -> ");
             System.out.println(eNature);
         }
         try {
-            new models.Gravites("bénin", null);
-            new models.Gravites("dangereux", null);
-            new models.Gravites("grave", null);
-            new models.Gravites("effroyable", null);
-            new models.Gravites("maléfique", null);
+            new models.Gravites("bénin", null).save();
+            new models.Gravites("dangereux", null).save();
+            new models.Gravites("grave", null).save();
+            new models.Gravites("effroyable", null).save();
+            new models.Gravites("maléfique", null).save();
         }catch (Exception eGrav){
             System.out.println("Erreur Création Gravité -> ");
             System.out.println(eGrav);
