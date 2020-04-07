95c62d887095271d799c7c096098c96db452f3fe
MaatheN <contact.pro@mathieu-neveu.fr>
2020-04-07 15:35:46 +0200

    ++ Ajoutgens FakeData_Civ James Olsen

--- app/controllers/Ajoutgens.java
+++ app/controllers/Ajoutgens.java
@@ -13,9 +13,15 @@ public class Ajoutgens extends Controller {
         try {
             new models.SuperH("SHK001001HNKDEFR","SuperMan","Kryptonite",'h',"toor").save();
             new models.SuperH("SVK001001HBEAPGQ","Général Zod","Kryptonite verte",'v',"toor").save();
-        }catch (Exception e){
+        }catch (Exception eSuper){
             System.out.println("Erreur Création Super -> ");
-            System.out.println(e);
+            System.out.println(eSuper);
+        }
+        try {
+            new models.Civil("CI-T001996F-7I5H4W",0,"Bartholomew Olsen","James","h","935963358","The Daily Planet","Metropolis","62960","j.olsen@thedailyplanet.com",null,null,"USA", new Date(),"toor").save();
+        }catch (Exception eCivil){
+            System.out.println("Erreur Création Civil -> ");
+            System.out.println(eCivil);
         }
         render();
     }
