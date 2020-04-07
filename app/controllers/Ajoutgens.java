522b0a7e17762c82c231060c15961f58e5d43cfa
MaatheN <contact.pro@mathieu-neveu.fr>
2020-04-07 14:25:59 +0200

    update Ajoutgens.java Insertbdd

--- app/controllers/Ajoutgens.java
+++ app/controllers/Ajoutgens.java
@@ -10,7 +10,12 @@ import models.*;
 public class Ajoutgens extends Controller {
 
     public static void index() {
-        new models.Civil("CI-T001976F-7I5H4W",0,"Jean","Civil","h","06667634","10 rue de la Motte","Raccourcis-Ville","66600","jean.c@gouv.fr",null,null,"France",null,"toor");
+        try {
+            new models.Civil("CI-T001996F-7I5H4W",0,"Jean","Civil","h","06667634","10 rue de la Motte","Raccourcis-Ville","66660","jean.c@gouv.fr",null,null,"France",null,"toor").save();
+        }catch (Exception e){
+            System.out.println("DEBUG : ERREUR ->");
+            System.out.println(e);
+        }
         new models.SuperH("SH-T001245H-LQ65XF","SuperMan","Cryptonite",'g',"toor");
         render();
     }
