aadaf5d89c81aa2233d9f1314d9d0c1de40adef7
Vaala <brice.bitot@neuf.fr>
2020-04-07 10:44:36 +0200

    Connexion controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,28 +7,7 @@ import java.util.*;
 
 import models.*;
 
-//@With(Secure.class)
-public class Application extends Controller {
-
-    @Before
-    static void setConnectedUser() {
-        if(Security.isConnected()) {
-            switch (Security.connected().substring(0, 2)){
-                case "SH":
-                    SuperH superh = SuperH.find("byId", Security.connected()).first();
-                    renderArgs.put("id", superh.id);
-                    renderArgs.put("username", superh.nom);
-                    renderArgs.put("admin", 2);
-                    break;
-                case "CI":
-                    Civil civil = Civil.find("byId", Security.connected()).first();
-                    renderArgs.put("id", civil.id);
-                    renderArgs.put("username", civil.nom + " " + civil.prenom);
-                    renderArgs.put("admin", civil.id_admin);
-                    break;
-            }
-        }
-    }
+public class Application extends ConnectionController {
 
     public static void index() {
         render();
