06ad6c7a8dfed4119dabe4d32cc87d5f4778df3a
Elise <elise.labbe@gmail.com>
2020-04-08 17:15:51 +0200

    suivi incident

--- app/controllers/Incidents.java
+++ app/controllers/Incidents.java
@@ -1,6 +1,5 @@
 package controllers;
 
-
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -20,11 +19,18 @@ public class Incidents extends ConnectionController {
         String longitude = params.get("lon");
         String latitude = params.get("lat");
         SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd");
+
         try {
-            new models.Incidents(Security.connected(), type, description, latitude, longitude, format.parse(date)).save();
+            new models.Incidents(Security.connected(), type, description, latitude, longitude, format.parse(date), false).save();
         } catch (java.text.ParseException ex){
             ex.fillInStackTrace();
         }
         redirect("/incident/new");
+    }
+
+    public static void suivi() {
+        List<models.Incidents> incidents = models.Incidents.find("byCivil", Security.connected()).fetch();
+        renderArgs.put("incidents", incidents);
+        render();
     }
 }
--- app/models/Incidents.java
+++ app/models/Incidents.java
@@ -15,18 +15,21 @@ public class Incidents extends Model {
     public String longitude;
     public String latitude;
     public Date date;
+    public boolean etat;
 
     public Incidents(String civil,
                      String type,
                      String description,
                      String latitude,
                      String longitude,
-                     Date date) {
+                     Date date,
+                     Boolean etat) {
         this.civil = civil;
         this.type = type;
         this.description = description;
         this.longitude = longitude;
         this.latitude = latitude;
         this.date = date;
+        this.etat = etat;
     }
 }
