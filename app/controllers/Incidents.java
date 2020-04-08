6998080ee50ab42f82aea6ac26483677de28b6d0
Vaala <brice.bitot@neuf.fr>
2020-04-08 14:23:58 +0200

    it works

--- app/controllers/Incidents.java
+++ app/controllers/Incidents.java
@@ -2,12 +2,20 @@ package controllers;
 
 
 import java.util.*;
+import play.Logger;
 
 public class Incidents extends ConnectionController {
-    static void getData() {
+
+    public static void index(){
+        render();
+    }
+
+
+    public static void postIndex() {
         String date = params.get("date");
         String description = params.get("description");
         String longitude = params.get("longitude");
         String latitude = params.get("latitude");
+        render();
     }
 }
