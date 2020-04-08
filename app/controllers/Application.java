567fef0c68e371f9986d1473390c240595e68c47
Vaala <brice.bitot@neuf.fr>
2020-04-08 10:45:08 +0200

    profil Super

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,21 +10,25 @@ import models.*;
 
 public class Application extends ConnectionController {
 
-    public static void index2() {
+    public static void index() {
         render();
     }
 
-    public static void profile(String id) {
-        switch (id.substring(0, 2)){
+    public static void profil(String id_profil) {
+        switch (id_profil.substring(0, 2)){
             case "SH":
-                SuperH superh = SuperH.find("byId", id).first();
-                List<Mission> missions = Mission.find("from Mission where id in (select id_mission from Assigner where id_super LIKE ?1)", id).fetch(0, 10);
+                SuperH superh = SuperH.find("byId", id_profil).first();
+                List<Mission> missions = Mission.find("from Mission where id in (select id_mission from Assigner where id_super LIKE ?1)", id_profil).fetch(0, 10);
+                List<Pouvoirs> pouvoirs = Mission.find("from Pouvoirs where id in (select id_pouvoir from Posseder where id_super LIKE ?1)", id_profil).fetch();
                 renderArgs.put("pointFaible", superh.pointFaibles);
                 renderArgs.put("type", superh.type);
                 renderArgs.put("missions", missions);
+                renderArgs.put("pouvoirs", pouvoirs);
+                renderArgs.put("id_profil", id_profil);
                 break;
             case "CI":
-                Civil civil = Civil.find("byId", id).first();
+                Civil civil = Civil.find("byId", id_profil).first();
+                renderArgs.put("civil", civil);
                 break;
         }
         render();
