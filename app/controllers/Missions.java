b6a70618064dff7f66e4e4b15a3b938cbe41e2e9
MaatheN <contact.pro@mathieu-neveu.fr>
2020-04-08 14:32:40 +0200

    ++ view & controller listing Missions

--- app/controllers/Missions.java
+++ app/controllers/Missions.java
@@ -1,10 +1,19 @@
 package controllers;
 
+import models.Gravites;
+import models.Mission;
+import models.NatureMission;
 import play.mvc.Controller;
 
-public class Missions extends ConnectionController {
-    public static void index(){
-        render();
-    }
+import java.util.List;
 
+public class Missions extends ConnectionController {
+
+
+    public static void index(){
+        List<Mission> missions = Mission.find("from Mission").fetch(0, 50);
+        List<NatureMission> natures = NatureMission.find("from NatureMission").fetch(0,10);
+        List<Gravites> gravites = Gravites.find("from Gravites").fetch(0,10);
+        render(missions,natures,gravites);
+    }
 }
