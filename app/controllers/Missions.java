509c6daac30c8a717bbdc714a090cfd222839606
MaatheN <contact.pro@mathieu-neveu.fr>
2020-04-08 19:47:02 +0200

    Update render info

--- app/controllers/Missions.java
+++ app/controllers/Missions.java
@@ -17,7 +17,27 @@ public class Missions extends ConnectionController {
         render(missions,natures,gravites);
     }
 
-    public static void info(String id_mission){
-        render();
+    public static void info(long id_mission){
+        List<Mission> mission = Mission.find("from Mission where id=?1", id_mission).fetch(0,1);
+
+        String missionTitre = mission.get(0).titre;
+        char missionReuss = mission.get(0).reussite;
+        String missionRayon = mission.get(0).rayon;
+        boolean missionUrg = mission.get(0).urgence;
+        List<Gravites> gravites = Gravites.find("from Gravites").fetch(0,10);
+        String nomGravite = null;
+        for (Gravites gravite : gravites){
+            if (gravite.id == mission.get(0).id_gravite){
+                nomGravite = gravite.nom;
+            }
+        }
+        List<NatureMission> natures = NatureMission.find("from NatureMission").fetch(0,10);
+        String nomNature = null;
+        for (NatureMission nature : natures){
+            if (nature.id == mission.get(0).id_nature){
+                nomNature = nature.nom;
+            }
+        }
+        render(missionTitre, id_mission, nomGravite, nomNature, missionReuss, missionRayon, missionUrg);
     }
 }
