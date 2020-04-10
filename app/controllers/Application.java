8fa22ece1dae0142234cbcbde4cf7dc4367c1178
Vaala <brice.bitot@neuf.fr>
2020-04-10 12:49:53 +0200

    fix profil

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -25,6 +25,7 @@ public class Application extends ConnectionController {
         switch (id_profil.substring(0, 2)){
             case "SH":
                 SuperH superh = SuperH.find("byId", id_profil).first();
+                Civil identite = Civil.find("byId", superh.id_civil).first();
                 List<Mission> missions = Mission.find("from Mission where id in (select id_mission from Assigner where id_super LIKE ?1)", id_profil).fetch(0, 10);
                 List<Pouvoirs> pouvoirs = Mission.find("from Pouvoirs where id in (select id_pouvoir from Posseder where id_super LIKE ?1)", id_profil).fetch();
                 renderArgs.put("pointFaible", superh.pointFaibles);
@@ -32,6 +33,7 @@ public class Application extends ConnectionController {
                 renderArgs.put("missions", missions);
                 renderArgs.put("pouvoirs", pouvoirs);
                 renderArgs.put("id_profil", id_profil);
+                renderArgs.put("identite", identite);
                 break;
             case "CI":
                 Civil civil = Civil.find("byId", id_profil).first();
